d82449390b3e562e99a6d0d5fd554b90f62990bb
Viacheslav Volodin <volodin28@gmail.com>
2023-05-04 23:12:43 +0300

    added teacher-group relationship

--- mysite/group/models.py
+++ mysite/group/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from teacher.models import Teacher
+
 
 class Group(models.Model):
     group_name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
--- mysite/teacher/models.py
+++ mysite/teacher/models.py
@@ -7,3 +7,6 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birthday = models.DateField(default=date.today)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
